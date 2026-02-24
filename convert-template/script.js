df9d33e9d49fc4a4f22d6fd82bde13fa5ee718e0
oZiel <115552341+henrixyz@users.noreply.github.com>
2026-02-24 09:02:54 -0300

    Add files via upload

---
+++ convert-template/script.js
@@ -0,0 +1,59 @@
+const form = document.querySelector("form");
+const numberReal = document.querySelector("#amount");
+const select = document.querySelector("select");
+const currentValueReal = 1;
+const footer = document.querySelector("footer");
+const description = document.querySelector("#description");
+const result = document.querySelector("#result");
+
+let currentUSD = 5.17;
+let currentGBP = 6.98;
+let currentEUR = 6.10;
+
+function convertToReal(type){
+    let real = Number(numberReal.value);
+    return real * type;
+}
+
+numberReal.addEventListener("input", () =>{
+    const regex = /\D+/g
+    numberReal.value = String(numberReal.value).replace(regex, "");
+})
+
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault()
+
+    try {
+        let convert = 0;
+    
+        if (select.value === "USD"){
+            convert = convertToReal(currentUSD);
+            description.innerText = `$1 = R$${currentUSD}`
+    
+        }else if(select.value === "GBP"){
+            convert = convertToReal(currentGBP);
+            description.innerText = `£1 = R$${currentGBP}`
+            
+        }else{
+            convert = convertToReal(currentEUR);
+            description.innerText = `€1 = R$${currentEUR}`
+        }
+    
+        result.innerText = convert.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+        footer.style.display = "block";
+
+    } catch (error) {
+        let real = Number(numberReal.value);
+
+        if (!real) {
+            alert("Digite um valor válido");
+            return;
+        }
+    }
+    
+})
+
