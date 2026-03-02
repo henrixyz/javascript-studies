f08ab014d5e7eaf5eafc29698fba2dc9cf6c9be1
oZiel <115552341+henrixyz@users.noreply.github.com>
2026-03-02 19:30:52 -0300

    Add files via upload

---
+++ fazeno/script.js
@@ -0,0 +1,70 @@
+// primeiro passo adicionar na lista
+const ativities = [];
+const btn_add = document.querySelector("#addBtn");
+const input = document.querySelector("#tarefaInput");
+const ul_list = document.querySelector("#listaTarefas");
+
+
+btn_add.addEventListener("click", () =>{
+
+    if (input.value.length === 0){
+        alert("digite algo")
+        return
+    }
+    
+    ativities.push({
+        text: input.value,
+        concluido: false
+    })
+    
+    input.value = ""
+    render()
+})
+
+input.addEventListener("keydown", (event)=>{
+    if (event.key === "Enter"){
+        btn_add.click()
+    }
+})
+
+function render(){
+    ul_list.innerHTML = ""
+
+    ativities.forEach((prop, index) => {
+
+    ul_list.innerHTML += `
+        <li
+        class="tarefa"
+        data-index= "${index}"
+        >
+            <span class="${prop.concluido ? "concluida" : "" }">${prop.text}</span>
+            <i class="fa-solid fa-xmark remover"></i>
+        </li>`
+    })
+
+    marcarAtividade()
+}
+
+// segundo passo marcar como concluida
+
+function marcarAtividade(){
+
+    const tarefas = document.querySelectorAll(".tarefa");
+    
+    tarefas.forEach((tarefa) =>{
+
+        tarefa.addEventListener("dblclick", () => {
+            console.log("deu aqui");
+            const index = tarefa.dataset.index;
+            ativities[index].concluido = !ativities[index].concluido;
+
+            render();
+
+        })
+    })
+
+}
+
+
+
+// terceiro apagar da lista
