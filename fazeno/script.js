27243a4a425c45fa1a24e9a141012c47264482f2
oZiel <115552341+henrixyz@users.noreply.github.com>
2026-03-03 20:33:25 -0300

    add func remove - fazeno

--- fazeno/script.js
+++ fazeno/script.js
@@ -6,7 +6,7 @@ const ul_list = document.querySelector("#listaTarefas");
 
 
 btn_add.addEventListener("click", () =>{
-
+    
     if (input.value.length === 0){
         alert("digite algo")
         return
@@ -29,33 +29,33 @@ input.addEventListener("keydown", (event)=>{
 
 function render(){
     ul_list.innerHTML = ""
-
+    
     ativities.forEach((prop, index) => {
-
-    ul_list.innerHTML += `
+        
+        ul_list.innerHTML += `
         <li
         class="tarefa"
         data-index= "${index}"
         >
-            <span class="${prop.concluido ? "concluida" : "" }">${prop.text}</span>
-            <i class="fa-solid fa-xmark remover"></i>
+        <span class="${prop.concluido ? "concluida" : "" }">${prop.text}</span>
+        <i class="fa-solid fa-xmark remover"></i>
         </li>`
     })
 
+    apagarAtividade()
     marcarAtividade()
 }
 
 // segundo passo marcar como concluida
 
 function marcarAtividade(){
-
+    
     const tarefas = document.querySelectorAll(".tarefa");
     let last_tap = 0
     
     tarefas.forEach((tarefa) =>{
-
+        
         tarefa.addEventListener("dblclick", () => {
-            console.log("deu aqui");
             const index = tarefa.dataset.index;
             ativities[index].concluido = !ativities[index].concluido;
             
@@ -72,14 +72,29 @@ function marcarAtividade(){
                 ativities[index].concluido = !ativities[index].concluido;
                 render()
             }
-
+            
             last_tap = now
         }) 
+        
+    })
+    
+}
 
+function apagarAtividade(){
+
+    const btn_remover = document.querySelectorAll(".remover");
+
+    btn_remover.forEach((btn)=>{
+
+        btn.addEventListener("click", (e) =>{
+
+            const index = e.target.closest(".tarefa").dataset.index
+            ativities.splice(index, 1)
+            render()
+        })
+        
     })
 
 }
 
 
-
-// terceiro apagar da lista
